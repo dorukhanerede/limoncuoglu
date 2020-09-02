183b085f85fef4d0f1a98efb96dd061bcb9e07f2
dorukhanerede <dorukhanerede@hotmail.com>
2020-09-03 01:31:43 +0300

    Update team area to reflect the database

--- src/components/TeamArea.js
+++ src/components/TeamArea.js
@@ -1,145 +1,114 @@
 import React from "react";
+import firebase from "firebase";
 
 class TeamArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+
+      documents: {},
+      images: {},
+    };
+  }
+
+  componentWillMount() {
+    this.getFirestore();
+    this.getStorage();
+  }
+
+  async getFirestore() {
+    await firebase
+      .firestore()
+      .collection("ekibimiz")
+      .orderBy("sectionData.order", "asc")
+      .get()
+      .then((collection) => {
+        let collections = {};
+        collection.docs.forEach((doc) => {
+          const data = doc.data();
+          collections = { ...collections, [doc.id]: data };
+        });
+        this.setState({ documents: collections });
+      });
+  }
+
+  async getStorage() {
+    const ref = firebase.storage().ref("ekibimiz");
+    let images = {};
+    await ref.listAll().then((result) => {
+      result.items.forEach((element) => {
+        element
+          .getDownloadURL()
+          .then((img) => {
+            const elementName = element.name.slice(
+              0,
+              element.name.lastIndexOf(".")
+            );
+            images = { ...images, [elementName]: img };
+          })
+          .then(() => {
+            this.setState({ images: images });
+          });
+      });
+    });
+  }
+
   render() {
-    let header;
-
-    let founders = [
-      {
-        tr: {
-          name: "İbrahim Önder Limoncuoğlu",
-          title: "Avukat",
-          email: "mail@mail.com",
-        },
-        en: {
-          name: "İbrahim Önder Limoncuoğlu",
-          title: "Attorney",
-          email: "mail@mail.com",
-        },
-        fr: {
-          name: "İbrahim Önder Limoncuoğlu",
-          title: "FR Avukat",
-          email: "mail@mail.com",
-        },
-      },
-    ];
-
-    let foundingSectionTitle;
-    let attorneySectionTitle;
-    let partnerSectionTitle;
-
-    let partners = [
-      {
-        tr: {
-          name: "Sabit Emre Limoncuoğlu",
-          title: "Avukat",
-          email: "mail@mail.com",
-          info:
-            "1993 yılında 9 Eylül Üniversitesi Hukuk Fakültesi’nden mezun olmuştur. 1995 yılından itibaren Limoncuoğlu Hukuk Bürosu’nda fiili olarak avukatlık yapmaktadır.",
-          img: "img/team/3.png",
-        },
-        en: {
-          name: "Sabit Emre Limoncuoğlu",
-          title: "Attorney",
-          email: "mail@mail.com",
-          info:
-            "He graduated from 9 Eylül University Faculty of Law in 1993. Since 1995, he has been working as a lawyer at Limoncuoğlu Law Firm.",
-          img: "img/team/3.png",
-        },
-        fr: {
-          name: "Sabit Emre Limoncuoğlu",
-          title: "FR Avukat",
-          email: "mail@mail.com",
-          info:
-            "Il est diplômé de la 9 faculté de droit de l'Université d'Eylül en 1993. Depuis 1995, il travaille comme avocat au cabinet d'avocats Limoncuoğlu.",
-          img: "img/team/3.png",
-        },
-      },
-    ];
-
-    let attorneys = [
-      {
-        tr: {
-          name: "İlayda Melis Alan",
-          title: "Avukat",
-          email: "mail@mail.com",
-          info:
-            "2018 yılında İzmir Ekonomi Üniversitesi Hukuk Fakültesi’nden mezun olmuştur. 2019 yılından itibaren büromuzda fiilen avukatlık yapmaktadır.",
-          img: "img/team/2.png",
-        },
-        en: {
-          name: "İlayda Melis Alan",
-          title: "Attorney",
-          email: "mail@mail.com",
-          info:
-            "She graduated from Izmir University of Economics Faculty of Law in 2018. Since 2019, he has been practicing lawyer in our office.",
-          img: "img/team/2.png",
-        },
-        fr: {
-          name: "İlayda Melis Alan",
-          title: "FR Avukat",
-          email: "mail@mail.com",
-          info:
-            "Il est diplômé de la faculté de droit de l'Université d'économie d'Izmir en 2018. Depuis 2019, il travaille comme avocat dans notre cabinet.",
-          img: "img/team/2.png",
-        },
-      },
-      {
-        tr: {
-          name: "Yiğit Efe Limoncuoğlu",
-          title: "Stajyer Avukat",
-          email: "efelimcuoglu@gmail.com",
-          info:
-            "2019 yılında İstanbul Bilgi Üniversitesi Hukuk Fakültesinden mezun olmuştur. Öğrenimini İstanbul’da tamamladıktan sonra avukatlık stajı için İzmir’e dönmüştür. Avukatlık stajı ile beraber İzmir Ekonomi Üniversitesi Hukuk Fakültesi Özel Hukuk Yüksek Lisans Programına kaydolarak yüksek lisans eğitimi almaktadır.",
-          img: "img/team/efe2.jpeg",
-        },
-        en: {
-          name: "Yiğit Efe Limoncuoğlu",
-          title: "Intern Attorney",
-          email: "efelimcuoglu@gmail.com",
-          info:
-            "He graduated from Istanbul Bilgi University Faculty of Law in 2019. After completing his education in Istanbul, he returned to Izmir for his lawyer internship. Along with his law internship, he enrolled in Izmir University of Economics Faculty of Law, Private Law Graduate Program and received his master's degree.",
-          img: "img/team/efe2.jpeg",
-        },
-        fr: {
-          name: "Yiğit Efe Limoncuoğlu",
-          title: "FR Stajyer Avukat",
-          email: "efelimcuoglu@gmail.com",
-          info:
-            "Elle est diplômée de la faculté de droit de l'Université Bilgi d'Istanbul en 2019. Après avoir terminé ses études à Istanbul, il est retourné à Izmir pour son stage d'avocat. Parallèlement à son stage en droit, il s'est inscrit à la faculté de droit de l'Université d'économie d'Izmir, au programme d'études supérieures en droit privé et a obtenu sa maîtrise.",
-          img: "img/team/efe2.jpeg",
-        },
-      },
-    ];
-
-    switch (this.props.language) {
-      case "tr":
-        header = "Sorularınız için";
-        foundingSectionTitle = "Kurucumuz";
-        attorneySectionTitle = "Kadromuz";
-        partnerSectionTitle = "Partner";
-        break;
-
-      case "en":
-        header = "For Any Information";
-        foundingSectionTitle = "Founder";
-        attorneySectionTitle = "Our Team";
-        partnerSectionTitle = "Partner";
-        break;
-
-      case "fr":
-        header = "fransızca";
-        foundingSectionTitle = "FR Kurucumuz";
-        attorneySectionTitle = "FR Kadromuz";
-        partnerSectionTitle = "FR Partner";
-        break;
-    }
-
+    let documentKeys = Object.keys(this.state.documents);
+    let documents = this.state.documents;
     return (
       <div className="team_area">
         <div className="container">
           <div className="border_bottom">
-            <div className="row">
+            {documentKeys.map((doc, i) => {
+              return (
+                <section key={doc}>
+                  <div className="row">
+                    <div className="col-xl-12">
+                      <div className="section_title mb-40 text-center">
+                        <h3>
+                          {documents[doc].sectionData[this.props.language]}
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row justify-content-around">
+                    {Object.keys(documents[doc]).map((data) => {
+                      const element = documents[doc][data][this.props.language];
+                      return data != "sectionData" ? (
+                        <div key={data} className="col-xl-4 col-lg-4 col-md-6">
+                          <div className="single_team">
+                            <div className="team_thumb">
+                              <img
+                                src={this.state.images[data]}
+                                alt={
+                                  "image_" +
+                                  data.split(" ").join("").toLocaleLowerCase()
+                                }
+                                style={{
+                                  objectFit: "cover",
+                                  width: "362px",
+                                  height: "400px",
+                                }}
+                              />
+                            </div>
+                            <div className="team_info text-center">
+                              <h3>{data}</h3>
+                              <p>{element.title}</p>
+                              <p className="text-left">{element.info}</p>
+                              <br></br>
+                              <p>{documents[doc][data].email}</p>
+                            </div>
+                          </div>
+                        </div>
+                      ) : null;
+                    })}
+                  </div>
+                </section>
+              );
+            })}
+            {/* <div className="row">
               <div className="col-xl-12">
                 <div className="section_title mb-40 text-center">
                   <h3>{foundingSectionTitle}</h3>
@@ -187,25 +156,6 @@ class TeamArea extends React.Component {
                         <p className="text-left">{element.info}</p>
                         <br></br>
                         <p>{element.email}</p>
-                        <div className="social_link">
-                          <ul>
-                            <li>
-                              <a href="#">
-                                <i className="fa fa-facebook" />
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i className="fa fa-twitter" />
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i className="fa fa-instagram" />
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
                       </div>
                     </div>
                   </div>
@@ -236,31 +186,12 @@ class TeamArea extends React.Component {
                         <p className="text-left">{element.info}</p>
                         <br></br>
                         <p>{element.email}</p>
-                        <div className="social_link">
-                          <ul>
-                            <li>
-                              <a href="#">
-                                <i className="fa fa-facebook" />
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i className="fa fa-twitter" />
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i className="fa fa-instagram" />
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
                       </div>
                     </div>
                   </div>
                 );
               })}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
